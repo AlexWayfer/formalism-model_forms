08168675c71641d5e3d7783f52e23a1d667ee5cf
Alexander Popov <alex.wayfer@gmail.com>
2020-07-10 01:03:33 +0300

    Update version to 0.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## master (unreleased)
+
+## 0.1.0 (2020-07-10)
+
+*   Initial release
--- lib/formalism/model_forms/version.rb
+++ lib/formalism/model_forms/version.rb
@@ -2,6 +2,6 @@
 
 module Formalism
 	module ModelForms
-		VERSION = '0.0.0'
+		VERSION = '0.1.0'
 	end
 end
